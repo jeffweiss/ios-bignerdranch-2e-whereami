74efb2fc1b69bc2104774ca51c56483af452fc11
Jeff Weiss <jeff.a.weiss@gmail.com>
2012-01-10 22:25:12 -0800

    Initial Commit

---
+++ Whereami/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Whereami
+//
+//  Created by Jeff Weiss on 1/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
